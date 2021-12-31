1dd7c38fc823b207a38cc8401c1d6a5c9f9f1018
Karl Lukan <klukan20@gmail.com>
2021-12-30 20:28:59 -0600

    UI elements are now data binded.

---
+++ MootraWeb/Emotion.cs
@@ -0,0 +1,18 @@
+﻿namespace MootraWeb
+{
+    /// <summary>
+    /// The class that represents an emotion.
+    /// </summary>
+    public class Emotion
+    {
+        /// <summary>
+        /// Gets or sets the emotion name.
+        /// </summary>
+        public string? Name { get; set; }
+
+        /// <summary>
+        /// Gets or sets the date of creation.
+        /// </summary>
+        public DateTime DateCreated { get; set; }
+    }
+}
